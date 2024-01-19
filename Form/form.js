2bc7f4e0eb7b4f31908f8463643e9fd40134f602
Vigneshd705 <vigneshd705@gmail.com>
2024-01-19 23:25:25 +0530

    Form

---
+++ Form/form.js
@@ -0,0 +1,7 @@
+function getinput(){
+    var name=document.getElementById("Name").value;
+    var age=document.getElementById("Age").value;
+    var email=document.getElementById("Email").value;
+    var DOB=document.getElementById("dob").value;
+    alert("Name: "+name+ "\nAge: "+age);
+}
